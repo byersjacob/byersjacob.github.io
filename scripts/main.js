822653fcad18db00bd5b148f1e59dc65f6a93bea
Jacob Byers <byers.jacob@gmail.com>
2018-10-10 16:05:13 -0700

    Testing combining script tags

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,5 @@
 $(window).on("load, resize",function(){$(window).width()<600&&$(".logoimg").removeClass("mr-auto").addClass("mx-auto")});
+
+function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","UA-87909907-2");
+
+var shiftWindow=function(){scrollBy(0,-100)};function load(){window.location.hash&&shiftWindow()}window.addEventListener("hashchange",shiftWindow);
